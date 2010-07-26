e634a767411fdc82ce350f15fdeb540e626af261
Leon Bollerup <mail@r2x2.com>
2010-07-26 10:54:11 +0200

    Updated the onclick event again, since the last broke the clock

--- core/kernel.sidebar.js.inc.php
+++ core/kernel.sidebar.js.inc.php
@@ -83,9 +83,8 @@ kernel_sidebar.prototype.addWidget = function(type,position,width,height,bottom,
 		tmp.style.zIndex					= '3';
 		tmp.style.width						= width;
 		tmp.style.bottom					= bottom;
-		tmp.onclick 							= sidebarCalendar.cycle();
 		tmp.style.right						= right;
-		tmp.innerHTML							= '<img id="sidebarClock" border="0" src="images/sidebar/clockbg.png" style="position: absolute; z-index: 3; bottom: 0px; right: 0px;">'+
+		tmp.innerHTML							= '<img onclick="sidebarCalendar.cycle();" id="sidebarClock" border="0" src="images/sidebar/clockbg.png" style="position: absolute; z-index: 3; bottom: 0px; right: 0px;">'+
 																'<object onclick="sidebarCalendar.cycle();" classid="clsid:D27CDB6E-AE6D-11CF-96B8-444553540000" style="position: absolute; z-index: 4; bottom: 22px; right: 26px;" id="obj1" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" border="0" width="73" height="73"><param name="movie" value="flash/clock.swf"><param name="quality" value="High"><param name="wmode" value="transparent"><embed src="flash/clock.swf" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj1" width="73" height="73" quality="High" wmode="transparent"></object>'+
 																'<div id="sidebarClock2" style="position: absolute; z-index: 3; bottom: 4px; right: 30px;"><font color="#E2EBFE" face="arial" size="1"><span id="sidebarClock2Digital"></span><span id="sidebarClock2Digital2"></span></font></div>';	
 		sidebar.appendChild(tmp);
