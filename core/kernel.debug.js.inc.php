26c187a057964bbab077daefaa748b1ed87adf63
Dans 34 <danarnell@gmail.com>
2010-08-01 11:47:10 +0100

    Slight Capitals Correction

--- core/kernel.debug.js.inc.php
+++ core/kernel.debug.js.inc.php
@@ -8,7 +8,7 @@ kernel_debug.prototype.create = function() {
 			tmp.id = "debugConsoleDIV";
 			tmp.className				= 'debugconsole';
 			tmp.style.backgroundImage = "url('images/debugconsole/background.png')";
-			tmp.innerHTML = '<table border="0" width="100%" id="debugconsoleTable" height="100%" cellpadding="0" style="border-collapse: collapse"><tr><td width="5" height="5"></td><td height="5"><font color="#FFFFFF face=" size="2" face="Arial">Longhorn debug console</font></td><td width="5" height="5"></td></tr><tr><td width="5">&nbsp;</td><td><form name="debug"><textarea DISABLED rows="10" name="console" cols="49" style="width:515px; height: 290px; font-family:Arial; font-size:8pt"></textarea></form></td><td width="5">&nbsp;</td></tr><tr><td width="5" height="5"></td><td height="5"></td><td width="5" height="5"></td></tr></table>';
+			tmp.innerHTML = '<table border="0" width="100%" id="debugconsoleTable" height="100%" cellpadding="0" style="border-collapse: collapse"><tr><td width="5" height="5"></td><td height="5"><font color="#FFFFFF face=" size="2" face="Arial">Longhorn Debug Console</font></td><td width="5" height="5"></td></tr><tr><td width="5">&nbsp;</td><td><form name="debug"><textarea DISABLED rows="10" name="console" cols="49" style="width:515px; height: 290px; font-family:Arial; font-size:8pt"></textarea></form></td><td width="5">&nbsp;</td></tr><tr><td width="5" height="5"></td><td height="5"></td><td width="5" height="5"></td></tr></table>';
 			document.body.appendChild(tmp);
 }
 
